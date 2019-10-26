6ff61a650da15c701f9b79d4bcae401ac070027a
Michael J. Miller <commnerd@gmail.com>
2019-10-25 22:58:07 -0600

    Check tests written

--- src/resources/assets/ui/src/app/components/check/check.component.spec.ts
+++ src/resources/assets/ui/src/app/components/check/check.component.spec.ts
@@ -22,4 +22,41 @@ describe('CheckComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should start unchecked', () => {
+    component.ngOnInit();
+    expect((component as any)._checked).toEqual(false);
+  });
+
+  it('should be truthy when checked', async(() => {
+    component.ngOnInit();
+
+    let check = fixture.debugElement.nativeElement.querySelector('input');
+    check.click();
+
+    fixture.whenStable().then(() => {
+      expect((component as any)._checked).toEqual(true);
+    });
+  }));
+
+  it('should call handler when clicked', async(() => {
+    spyOn(component, 'handleChange');
+
+    component.ngOnInit();
+
+    let check = fixture.debugElement.nativeElement.querySelector('input');
+    check.click();
+
+    fixture.whenStable().then(() => {
+      expect(component.handleChange).toHaveBeenCalled();
+    });
+  }));
+
+  it('should emit update when clicked', () => {
+    (component as any)._emitter.subscribe(g => {
+      expect(g).toEqual(true);
+    });
+    component.ngOnInit();
+    component.handleChange();
+  });
 });
--- src/resources/assets/ui/src/app/components/check/check.component.ts
+++ src/resources/assets/ui/src/app/components/check/check.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'check',
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CheckComponent implements OnInit {
 
+  @Output() _emitter: EventEmitter<boolean> = new EventEmitter();
+  private _checked: boolean;
+
   constructor() { }
 
   ngOnInit() {
+    this._checked = false;
+  }
+
+  handleChange() {
+    this._checked = !this._checked;
+    this._emitter.emit(this._checked);
   }
 
 }
